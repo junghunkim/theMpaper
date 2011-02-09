e9f037308fd1bd556da46461320c9ed5164e2da3
Nam <thenaminator@ubuntu.ubuntu-domain>
2011-02-09 16:54:58 -0500

    201102090454PM

--- simulator_cpp.cpp
+++ simulator_cpp.cpp
@@ -1,5 +1,3 @@
-//mysig = signature(param="matrix", init="numeric", LHS="numeric", RHS="numeric")
-//myinc = '
 #include <cmath>   
 
 arma::mat Param = Rcpp::as<arma::mat>( Param_In ) ;
@@ -40,76 +38,61 @@ arma::mat LatentPosition::rawSimulator(double myLHS, double myRHS, arma::mat Ini
   double drift_term = 0;
   double volat_term = 0;
   
-  arma::mat States_VT(1+nvertex,ngrid);
-  arma::mat States_CU(Init);
+  arma::mat States_VT(1+nvertex,ngrid+1);
   
-  States_VT
-  for(int itr_t=0;itr_t < ngrid;itr_t++) {
-    for(int itr_v=0;itr_v <nvertex;itr_v++){
-      drift_term = Param(itr_v,1)*(States_CU(itr_v) - Param(itr_v,2))/ngrid;
-      volat_term = Param(itr_v,3)*sqrt(States_CU(itr_v)*(1-States_CU(itr_v))/ngrid)*as_scalar(arma::randn());
+  const double dT = (myRHS-myLHS)/ngrid;
+
+  States_VT(0,0) = myLHS;
+  States_VT(span(1,nvertex),0) = Init;
+
+  for(int itr_t=1;itr_t <= ngrid; itr_t++) {
+    States_VT(0,itr_t) = States_VT(0,itr_t-1) + dT; 
+    for(int itr_v=1;itr_v <= nvertex; itr_v++){
+      drift_term = Param(itr_v,1)*(States_VT(itr_v,itr_t-1) - Param(itr_v,0))*dT;
+      volat_term = Param(itr_v,2)*sqrt(States_VT(itr_v,itr_t)*(1-States_VT(itr_v,itr_t-1))*dT)*as_scalar(arma::randn());
       States_VT(itr_v,itr_t) = draft_term + volat_term; 
-    }
-    States_CU = States_VT(arma::span::all,itr_t);
-  }
+    };
+  };
+
   return(States_VT);
 }
 
-arma::mat Conditional_LatentPosition::RejectionSampling(){
-  arma::mat sim_out; //nrow == nvertex  && ncol = original ngrid?
+arma::mat Conditional_LatentPosition::rejSimulator(){
+  arma::mat sim_out; 
   bool do_more = true;
 
-  setLSHRHS(curlhs,currhs);
-  setInit(arma)
-  while(do_more){  
-
-    sim_out = Simulate(dT); //need to know scope resolution
+  for(int itr.message=0;itr.message<nmessages;itr.messages++){
+    double myLHS =;
+    double myRHS =;
+    arma::mat myInit=;
+    while(do_more){  
+      
+      sim_out = Simulate(myLHS,myRHS,myInit); 
+      
+      for(int i=1;i<nvertex;i++){
+	mean(sim_out,dim=1)*arma::sum(dT);
+      };
+      
+      for(int i=1;i<nvertex; i++){ //iterate through vertex
+	for(int j = (i+1); j <=nvertex;j++){ //iterate through diff vertex
+	  int temp_int = sim_out[i-1,j-1];
+	  if(temp_int == 1){ // 1 means topic one 
+	    temp_val_2 *= sim_out[i-1,t-1]*sim_out[j-1,t-1];
+	  } else if(temp_int == 2) {
+	    temp_val_2 *= (1-sim_out[i-1,t-1])*(1-sim_out[j-1,t-1]);
+	  } else {
+	    temp_val_2 *= 1;
+	  };	// then, need to know if two communicated and compute the weight here
+	}; 
+      };
+    };  
     
-    for(int i=1;i<nvertex;i++){
-      mean(sim_out,dim=1)*arma::sum(dT);
-    };
-
-    for(int i=1;i<nvertex; i++){ //iterate through vertex
-      for(int j = (i+1); j <=nvertex;j++){ //iterate through diff vertex
-	int temp_int = sim_out[i-1,j-1];
-	if(temp_int == 1){ // 1 means topic one 
-	  temp_val_2 *= sim_out[i-1,t-1]*sim_out[j-1,t-1];
-	} else if(temp_int == 2) {
-	  temp_val_2 *= (1-sim_out[i-1,t-1])*(1-sim_out[j-1,t-1]);
-	} else {
-	  temp_val_2 *= 1;
-	};	// then, need to know if two communicated and compute the weight here
-      }; 
-    };
-    
-
     if(){
       = T;
     };
   };
-
+  
 };
-
-void LatentPosition::setLHSRHS(double lhs, double rhs)
-{
-  this.lhs = lhs;
-  this.rhs = rhs;
-};
-
-void LatentPosition::setParam(arma::mat Param_in)
-{
-  this.Param = Param_in;
-  this.nparam = Param.n_cols;
-};
-
-void LatentPosition::setInit(arma::mat Init_in)
-{
-  this.Init = Init_in;
-  this.nvertex = Init_in.n_rows;
-}
-
-
-
 
 int main() {
   const int NVERTEX = 4;
