4600140666d43d9cfd88f4a2576f6b6975416144
nhleesd@gmail.com <thenaminator@ubuntu.ubuntu-domain>
2011-03-05 17:05:29 -0500

    myminsun

--- myEstimator.cpp
+++ myEstimator.cpp
@@ -18,6 +18,7 @@ private:
   const int nmessages;
 
   mat Param;
+  mat Rate;
   //mat Param_Poisson;
   const colvec Data_t;
   const colvec Data_k;
@@ -27,22 +28,18 @@ public:
   CLP(mat Data_in, int nParam_in, int nVertex_in);
   ~CLP();
   void setParam(mat Param_in);
+  void setRate(mat Rate_in);
   mat rawSimulator(double myLHS, double myRHS, colvec Init);
   mat rejSimulator();
-  colvec getMeans(int maxMC);
-  void ShowData();
+  colvec getEST(int maxMC);
 };
 
-void CLP::ShowData() {
-  cout << Data_t << endl;
-  cout << Data_v << endl;
-  cout << Data_k << endl;
-}
-
-colvec CLP::getMeans(int maxMC){
+colvec CLP::getEST(int maxMC){
   colvec retSta = zeros(nvertex);
-  double retSca = 0;
+  colvec retSca = zeros(1);
+  colvec retOBJ = zeros(nvertex+1);
 
+  colvec Ts;
   colvec dT;
   colvec row_i;
   colvec row_j;
@@ -52,14 +49,23 @@ colvec CLP::getMeans(int maxMC){
   for(int mc_itr = 0; mc_itr < maxMC; ++mc_itr){
     cout << "\t MC#= "<< mc_itr << endl;
     cur_sim = rejSimulator();
-    dT = trans(cur_sim.row(nvertex)); 
 
+    Ts = trans(cur_sim.row(nvertex));
+
+    dT = zeros<colvec>(Ts.n_elem);
+    for(int itr =1; itr < Ts.n_elem;++itr){
+      dT(itr) = Ts(itr) - Ts(itr-1);
+      cout << "HERE" << itr << endl;
+    }
+
+    
     retSta = retSta + mean(cur_sim.rows(0,nvertex-1),1);
 
     for(int i =0; i<nvertex-1;i++){	
-      for(int j=(i+1); j <nvertex;j++) {
+      for(int j=(i+1);j <nvertex;j++) {
 	row_i = trans(cur_sim.row(i));
 	row_j = trans(cur_sim.row(j));
+	cout <<"HERE"<<endl;        
 	retSca = retSca + sum((row_i % row_j + (1-row_i) % (1-row_j)) % dT)/nmessages;
       }
     }
@@ -67,14 +73,21 @@ colvec CLP::getMeans(int maxMC){
 
   retSta = retSta/maxMC;
   retSca = retSca/maxMC;
+  retOBJ = join_cols(retSta,retSca);
 
-  return(join_cols(retSta,retSca));
+  return(retOBJ);
 }
 
 void CLP::setParam(mat Param_in)
 {
   Param = Param_in;
 }
+
+void CLP::setRate(mat Rate_in)
+{
+  Rate = Rate_in;
+}
+
 
 CLP::~CLP(){
 }
@@ -117,8 +130,8 @@ mat CLP::rawSimulator(double myLHS, double myRHS, colvec Init)
 }
 
 mat CLP::rejSimulator(){
-  mat retOBJ(nvertex,1);
-  retOBJ(span::all,0) = Param(span(0,nvertex-1),0);
+  mat retSTATE = Param.col(0); //columns will grow...
+  mat retTIMES = zeros(1); //columns will grow...
   
   double myLHS_cur;
   double myRHS_cur;
@@ -135,7 +148,7 @@ mat CLP::rejSimulator(){
     myTOPIC_cur = Data_k(itr_message+1);
     
     if(itr_message == 0){
-      myINIT_cur = Param(span(0,nvertex-1),0);
+      myINIT_cur = Param.col(0);
     } else {
       // otherwise updated at the end of the while-loop below
     }
@@ -145,7 +158,7 @@ mat CLP::rejSimulator(){
     while(do_more){  
       mat retOBJ_prop = rawSimulator(myLHS_cur,myRHS_cur,myINIT_cur);
       mat State_VX_prop = retOBJ_prop.rows(0,nvertex-1);
-      //      mat State_T_prop = retOBJ_prop(nvertex,span::all);
+      mat State_T_prop = retOBJ_prop.row(nvertex);
 
       int myRHS_index = retOBJ_prop.n_cols-1;
       double mystop_prob = 0;
@@ -169,7 +182,9 @@ mat CLP::rejSimulator(){
 	  }
 
 	  mystop_prob = mystop_prob + log(weight);
-	  mystop_prob = mystop_prob - Param(nvertex,0)*mean(path_ij_rate_sum)*(myRHS_cur-myLHS_cur);
+
+	  mystop_prob = mystop_prob - Rate(0,0)*mean(path_ij_rate_sum)*(myRHS_cur-myLHS_cur);
+	 
 	} // j>i for-ends
       } // i for-ends
       //
@@ -177,8 +192,8 @@ mat CLP::rejSimulator(){
       if(as_scalar(log(randu(1))) < mystop_prob) {
 	do_more = false;
 	myINIT_cur = State_VX_prop.col(myRHS_index);	
-	retOBJ_prop = State_VX_prop.cols(1,myRHS_index);
-	retOBJ = join_rows(retOBJ,retOBJ_prop);
+	retSTATE = join_rows(retSTATE,State_VX_prop.cols(1,myRHS_index));
+	retTIMES = join_rows(retTIMES,State_T_prop.cols(1,myRHS_index));
       } else {
 	do_more = true;
       }
@@ -188,7 +203,7 @@ mat CLP::rejSimulator(){
 
     // starting new interval
   }
-  return(retOBJ);
+  return(join_cols(retSTATE,retTIMES));
 }
 
 int main(int argc, char **argv) {
@@ -196,41 +211,49 @@ int main(int argc, char **argv) {
   // const int NVERTEX_Abnorm = 2;
   int NVERTEX = atoi(argv[1]);
   int NPARAM = atoi(argv[2]);
-
-  // mat normal_vertex_param(NVERTEX_Normal, NPARAM);
-  // mat abnorm_vertex_param(NVERTEX_Abnorm, NPARAM);
-  mat myParam(NVERTEX,NPARAM);
-  
-  mat tmpmat;
-  tmpmat.load("SIMIN.mat", raw_ascii);
-  cout << tmpmat;
-  myParam.col(0) = trans(tmpmat.row(tmpmat.n_rows - 1));
-  myParam.col(1) = ones(NVERTEX)*(-1);
-  myParam.col(2) = ones(NVERTEX);
-  cout << myParam;
-  // normal_vertex_param << 0.5 << -1 << 1 << endr << 0.5 << -1 << 1 << endr;
-  // abnorm_vertex_param << 0.5 << -1 << 1 << endr << 0.5 << -1 << 1 << endr;
-  // cout << "Test 1" << endl;
-  
-  // myParam = join_cols(normal_vertex_param,abnorm_vertex_param);
-  //  cout << "Test 2" << endl;
- mat myData;
-  myData.load("myData.txt",raw_ascii);
-  cout << myData;
-  CLP myCLP(myData, NPARAM, NVERTEX);
-  myCLP.setParam(myParam);
- 
   int maxSearch = atoi(argv[3]);
   int maxMC = atoi(argv[4]);
 
-  mat SaveME(maxSearch,NVERTEX);
+  mat myParam(NVERTEX,NPARAM);
+  mat myRate(1,1);
+  
+  mat SIMIN;
+  SIMIN.load("SIMIN.mat", raw_ascii);
+  myParam.col(0) = trans(SIMIN(SIMIN.n_rows - 1, span(0,NVERTEX-1)));
+  myParam.col(1) = ones(NVERTEX)*(-1);
+  myParam.col(2) = ones(NVERTEX);
+  myRate = SIMIN(SIMIN.n_rows-1,NPARAM);
+
+  mat myData;
+  myData.load("myData.txt",raw_ascii);
+
+  cout << myData <<endl;
+  cout << myParam << endl;
+  cout << myRate << endl;
+
+  CLP myCLP(myData, NPARAM, NVERTEX);
+  myCLP.setParam(myParam);
+  myCLP.setRate(myRate);
+ 
+  mat SaveME(maxSearch,NVERTEX+1);
+  mat myITRresult; 
+  
   for(int i=0;i < maxSearch; ++i){
     cout << "Search #" << i << endl;
-    myParam.col(0) = myCLP.getMeans(maxMC);
+
+    myITRresult = myCLP.getEST(maxMC);    
+
+
+    myParam.col(0) = myITRresult(span(0,NVERTEX-1),0);
     myCLP.setParam(myParam);
-    SaveME.row(i) = reshape(myParam.col(0),1,NVERTEX);
-    cout << reshape(myParam.col(0),1,NVERTEX) << endl;
+    myCLP.setRate(ones(1)*myITRresult(NVERTEX,0));
+    
+    SaveME.row(i) = reshape(myITRresult,1,NVERTEX+1);
+    
+    cout << SaveME.row(i) << endl;
   }
+  
   SaveME.save("SIMOUT.mat",raw_ascii);
- return 0;
+  
+  return 0;
 }
